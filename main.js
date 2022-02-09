fe861fa8dbfceeac091fa81a6f95b577e5217d65
Mohamed Nououali <mohamed.nououali@gmail.com>
2022-02-09 17:10:43 +0100

    Fine esercizio

---
+++ main.js
@@ -0,0 +1,9 @@
+const root = new Vue (
+    {
+        el : "#root",
+        data: {
+            messaggio: "Ciao Vue",
+            immagine:"https://d585tldpucybw.cloudfront.net/sfimages/default-source/default-album/870x220_telerik-blogsce2fdf065dfe46a2b2054ccc57d2d18d.png?sfvrsn=222ff053_1",
+        }
+    }
+);
